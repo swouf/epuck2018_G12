10d7befac2c2ac0d3696529d789aa86042d496e8
jeremy <jeremy.jayet@epfl.ch>
2018-03-27 14:10:28 +0200

    Mise en place des fichiers de base

---
+++ main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <chprintf.h>
+#include <motors.h>
+
+//uncomment to send the FFTs results from the real microphones
+//#define SEND_FROM_MIC
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
+static void timer12_start(void){
+    //General Purpose Timer configuration   
+    //timer 12 is a 16 bit timer so we can measure time
+    //to about 65ms with a 1Mhz counter
+    static const GPTConfig gpt12cfg = {
+        1000000,        /* 1MHz timer clock in order to measure uS.*/
+        NULL,           /* Timer callback.*/
+        0,
+        0
+    };
+
+    gptStart(&GPTD12, &gpt12cfg);
+    //let the timer count to max value
+    gptStartContinuous(&GPTD12, 0xFFFF);
+}
+
+int main(void)
+{
+
+    halInit();
+    chSysInit();
+    mpu_init();
+
+    //starts the serial communication
+    serial_start();
+    //starts the USB communication
+    usb_start();
+    //starts timer 12
+    timer12_start();
+    //inits the motors
+    motors_init();
+
+    while (1) {
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
---
+++ makefile
@@ -0,0 +1,20 @@
+
+#This is a template to build your own project with the e-puck2_main-processor folder as a library.
+#Simply adapt the lines below to be able to compile
+
+# Define project name here
+PROJECT = epuck2018_G12
+
+USE_VERBOSE_COMPILE=no
+
+#Define path to the e-puck2_main-processor folder
+GLOBAL_PATH = ../lib/e-puck2_main-processor
+
+#Source files to include
+CSRC += ./main.c
+
+#Header folders to include
+INCDIR += 
+
+#Jump to the main Makefile
+include $(GLOBAL_PATH)/Makefile
